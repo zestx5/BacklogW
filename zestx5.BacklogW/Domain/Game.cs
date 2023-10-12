fbc5a673f2e3926e2495ec95ec7bd88763f884d6
zestx5 <buyankov.ilya@gmail.com>
2023-10-12 16:31:09 +0300

    Add: Domain class Game

---
+++ zestx5.BacklogW/Domain/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zestx5.BacklogW.Domain
+{
+    public class Game
+    {
+        public Guid Guid { get; } = Guid.NewGuid();
+        public string Name { get; private set; } = string.Empty;
+        public string Notes { get; set; } = string.Empty;
+
+        public Game(string name, string notes)
+        {
+            Name = name;
+            Notes = notes;
+        }
+    }
+}
